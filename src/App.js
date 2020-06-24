f25e562e2b44b98158f491d0f6484dd8037cf737
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-06-24 07:40:57 -0500

    Understanding Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  return <h1 title="This works.">A React App!</h1>;
 };
+
+// class App extends React.Component {
+//   render(){
+//     return <h1 title="This works.">A React App with a class based component!</h1>;
+//   }
+// }          This is a class based component
 
 export default App;
