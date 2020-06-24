f514c77770002e5e2d6d6c740a23b649487120ac
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-06-24 09:37:51 -0500

    Rendering Lists of Data

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -3,10 +3,9 @@ import React from 'react';
 import './GoalList.css'
 
 const GoalList = props =>{
-    console.log(props.goals);
     return (
     <ul className="goal-list">
-   
+        {props.goals.map(goal=> <li key={goal.id}>{goal.text}</li>)}
     </ul>
   )
 }
