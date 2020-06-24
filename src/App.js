d01911ebdcd534d2d2889de4d52111ca62ef01aa
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-06-24 08:31:38 -0500

    Working with multiple components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './App.css';
+import GoalList from './components/GoalList'
 
 const App = () => {
-  return <h1 title="This works.">A React App!</h1>;
+  // return <h1 title="This works.">A React App!</h1>;
+  return <div className="course-goals">
+    <h2>Course Goals</h2>
+    {/* <ul className="goal-list">
+      <li>complete course</li>
+      <li>good understanding of couse material</li>
+      <li>help people in Q&amp;A</li>
+    {/* </ul> */}                           {/* if JSX was not used as seperate component */}
+    <GoalList />                            {/*  GoalList imported as component */}
+  </div>
 };
 
 // class App extends React.Component {
---
+++ src/components/GoalList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './GoalList.css'
+
+const GoalList = ()=>{
+    return (
+    <ul className="goal-list">
+    <li>complete course</li>
+    <li>good understanding of couse material</li>
+    <li>help people in Q&amp;A</li>
+    <li>pat self on back</li>
+    </ul>
+  )
+}
+
+export default GoalList;
+
