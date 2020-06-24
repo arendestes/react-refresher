5cc3d136a6f51ec75569ae8c7b04930ce900fba5
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-06-24 12:23:40 -0500

    Fetching user input (Two-waybinding)

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewGoal.css';
 
 const NewGoal = props => {
+
+    const [textInput, setTextInput] = useState('')
 
     const newGoalHandler = event =>{
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'new goal'
+            text: textInput
         }
-
+        setTextInput('');
         props.onAddGoal(newGoal);
         
     }
 
+    const textChangeHanler = event =>{
+        setTextInput(event.target.value);
+    }
+
     return <form className='new-goal' onSubmit={newGoalHandler}>
-        <input type="text"/>
+        <input type="text" value={textInput} onChange={textChangeHanler}/ >
         <button type='submit'>New Goal</button>
     </form>
 }
